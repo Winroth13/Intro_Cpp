16dfbaa5b0f4a11118b5ac3c6acb86004283f5ae
Emil Winroth <emil.winroth@gmail.com>
2025-02-06 15:09:31 +0100

    Task 4.3 Complete

--- module_2/quick_tasks/4.1/main.cpp
+++ module_2/quick_tasks/4.1/main.cpp
@@ -5,7 +5,9 @@
 int main() {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	Rectangle rectArr[3];
+	int nrOf = 3;
+
+	Rectangle* rectArr = new Rectangle[nrOf];
 
 	rectArr[0].setWidth(12);
 	rectArr[0].setHeight(15);
@@ -26,6 +28,23 @@ int main() {
 
 	doubleSides(rectArr[0]);
 	showMetrics(rectArr, 3);
+	std::cout << "\n";
+
+	nrOf = nrOf * 2;;
+
+	Rectangle* oldArr = rectArr;
+
+	rectArr = new Rectangle[nrOf];
+
+	for (int i = 0; i < nrOf; ++i) {
+		rectArr[i] = oldArr[i % 3];
+	}
+
+	delete[] oldArr;
+
+	showMetrics(rectArr, nrOf);
+
+	delete[] rectArr;
 
 	return 0;
 }
